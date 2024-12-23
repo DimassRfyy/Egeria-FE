8df262eaf41a890f2821b855746dc9340b65c460
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2024-12-23 09:30:04 +0700

    Create features to increase and decrease quantity

--- src/pages/MyCartPage.tsx
+++ src/pages/MyCartPage.tsx
@@ -62,6 +62,22 @@ export default function MyCartPage() {
   const tax = subTotal * 0.12;
   const grandTotal = subTotal + tax;
 
+  const handleIncreaseQuantity = (slug: string) => {
+    setCart((prevCart) => {
+      const updatedCart = prevCart.map((item) => (item.slug === slug && item.quantity < 10 ? { ...item, quantity: item.quantity + 1 } : item));
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+      return updatedCart;
+    });
+  };
+
+  const handleDecreaseQuantity = (slug: string) => {
+    setCart((prevCart) => {
+      const updatedCart = prevCart.map((item) => (item.slug === slug && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item));
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+      return updatedCart;
+    });
+  };
+
   const handleRemoveItem = (slug: string) => {
     const updatedCart = cart.filter((item) => item.slug !== slug);
     setCart(updatedCart);
@@ -127,11 +143,11 @@ export default function MyCartPage() {
                             </strong>
                           </p>
                           <div className="flex w-[89px] items-center justify-between gap-1 rounded-full bg-[#F6F6F8] px-2 py-[6px]">
-                            <button type="button">
+                            <button onClick={() => handleDecreaseQuantity(cosmetic.slug)}>
                               <img src="/assets/images/icons/min.svg" alt="icon" className="h-[21px] w-5 shrink-0" />
                             </button>
                             <p className="text-center text-sm font-semibold leading-[21px]">{cartItem?.quantity}</p>
-                            <button type="button">
+                            <button onClick={() => handleIncreaseQuantity(cosmetic.slug)}>
                               <img src="/assets/images/icons/plus.svg" alt="icon" className="h-[21px] w-5 shrink-0" />
                             </button>
                           </div>
@@ -144,66 +160,66 @@ export default function MyCartPage() {
           </div>
         </section>
         <section id="BookingDetails">
-          <form action="booking.html">
-            <div className="flex flex-col gap-5 rounded-t-[30px] bg-white px-5 pb-[30px] pt-[30px]">
-              <h2 className="font-bold">Booking Details</h2>
-              <div className="flex flex-col gap-[6px]">
-                <div className="relative h-[49px]">
-                  <input
-                    placeholder="Enter your discount code"
-                    type="text"
-                    className="absolute w-full rounded-full bg-[#F6F6F8] py-[14px] pl-4 pr-[92px] font-semibold text-[#030504] placeholder:text-sm placeholder:font-normal placeholder:leading-[21px] placeholder:text-cosmetics-grey focus:outline-none"
-                  />
-                  <button type="button" className="absolute right-[6px] top-1/2 -translate-y-1/2 rounded-full bg-cosmetics-purple px-[14px] py-2 text-sm font-semibold leading-[21px] text-white">
-                    Apply
-                  </button>
-                </div>
-                {/* <p className="text-sm leading-[21px] text-[#E70011]">Lorem tidak valid silahkan coba lagi ya</p> */}
+          <div className="flex flex-col gap-5 rounded-t-[30px] bg-white px-5 pb-[30px] pt-[30px]">
+            <h2 className="font-bold">Booking Details</h2>
+            <div className="flex flex-col gap-[6px]">
+              <div className="relative h-[49px]">
+                <input
+                  placeholder="Enter your discount code"
+                  type="text"
+                  className="absolute w-full rounded-full bg-[#F6F6F8] py-[14px] pl-4 pr-[92px] font-semibold text-[#030504] placeholder:text-sm placeholder:font-normal placeholder:leading-[21px] placeholder:text-cosmetics-grey focus:outline-none"
+                />
+                <button type="button" className="absolute right-[6px] top-1/2 -translate-y-1/2 rounded-full bg-cosmetics-purple px-[14px] py-2 text-sm font-semibold leading-[21px] text-white">
+                  Apply
+                </button>
               </div>
-              <div className="box h-[1px] w-full" />
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-[6px]">
-                  <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
-                  <p>Total Quantity</p>
-                </div>
-                <strong className="font-semibold">{totalQuantity} Items</strong>
+              {/* <p className="text-sm leading-[21px] text-[#E70011]">Lorem tidak valid silahkan coba lagi ya</p> */}
+            </div>
+            <div className="box h-[1px] w-full" />
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-[6px]">
+                <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
+                <p>Total Quantity</p>
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-[6px]">
-                  <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
-                  <p>Sub Total</p>
-                </div>
-                <strong className="font-semibold">{new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(subTotal)}</strong>
+              <strong className="font-semibold">{totalQuantity} Items</strong>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-[6px]">
+                <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
+                <p>Sub Total</p>
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-[6px]">
-                  <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
-                  <p>Discount Code</p>
-                </div>
-                <strong className="font-semibold">Rp 0</strong>
+              <strong className="font-semibold">{new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(subTotal)}</strong>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-[6px]">
+                <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
+                <p>Discount Code</p>
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-[6px]">
-                  <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
-                  <p>Tax 12%</p>
-                </div>
-                <strong className="font-semibold">{new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(tax)}</strong>
+              <strong className="font-semibold">Rp 0</strong>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-[6px]">
+                <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
+                <p>Tax 12%</p>
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-[6px]">
-                  <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
-                  <p>Grand Total</p>
-                </div>
-                <strong className="text-[22px] font-bold leading-[33px] text-cosmetics-pink">
-                  {new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(grandTotal)}
-                </strong>
+              <strong className="font-semibold">{new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(tax)}</strong>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-[6px]">
+                <img src="/assets/images/icons/note.svg" alt="icon" className="size-5 shrink-0" />
+                <p>Grand Total</p>
               </div>
-              <button type="submit" className="flex w-full items-center justify-between rounded-full bg-cosmetics-gradient-pink-white px-5 py-[14px] transition-all duration-300 hover:shadow-[0px_6px_22px_0px_#FF4D9E82]">
+              <strong className="text-[22px] font-bold leading-[33px] text-cosmetics-pink">
+                {new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(grandTotal)}
+              </strong>
+            </div>
+            {cart.length > 0 && (
+              <Link to="/booking" className="flex w-full items-center justify-between rounded-full bg-cosmetics-gradient-pink-white px-5 py-[14px] transition-all duration-300 hover:shadow-[0px_6px_22px_0px_#FF4D9E82]">
                 <strong className="font-semibold text-white">Continue Booking</strong>
                 <img src="/assets/images/icons/right.svg" alt="icon" className="size-[24px] shrink-0" />
-              </button>
-            </div>
-          </form>
+              </Link>
+            )}
+          </div>
         </section>
       </div>
     </main>
